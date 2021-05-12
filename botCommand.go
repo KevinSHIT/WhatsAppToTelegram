e8124dc1206ac46827aa272dd7c89d365cf1afb6
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-05-12 18:51:05 +0100

    add setnotify help

--- botCommand.go
+++ botCommand.go
@@ -13,18 +13,21 @@ func getResponse(msg string, jid string) string {
 			"Now the bot provides msg transferring function!\n" +
 			"The source code will be released on my GitHub account under OKZPL or MIT asap.\n" +
 			"If you are facing urgent situation, please send message through your real phone or call directly.\n" +
-			"Sorry for the inconvenience caused by this. Have a good day! :)"
+			"Sorry for the inconvenience caused by this. Have a good day! :)\n" +
+			"One more thing, here is the command list:\n" +
+			".help Get Help\n" +
+			".setnotify [false|true] Set the notify which after transferring. If the forward is failed, the notify will still be sent."
 	}
 	if strings.HasPrefix(strings.ToLower(msg), ".setnotify") {
 		b, err := strconv.ParseBool(strings.Trim(msg[10:], "x"))
-			if err != nil {
-				return "ERR: WA_P_TYPECOV"
-			}
-			if jid == "" {
-				return "ERR: WA_P_JID_EMPTY"
-			}
-			skipNotifyMap[jid] = !b
-			return fmt.Sprintf( "Success set Notify to %v", b)
+		if err != nil {
+			return "ERR: WA_P_TYPECOV"
+		}
+		if jid == "" {
+			return "ERR: WA_P_JID_EMPTY"
+		}
+		skipNotifyMap[jid] = !b
+		return fmt.Sprintf("Success set Notify to %v", b)
 	}
 	return "Sorry, cannot found the command!"
 
